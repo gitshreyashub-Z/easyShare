7eb4ffd04d1645ca93d6c56f2a2e9fa07b35dfbd
ghugeshreyashmnnit@gmail.com <ghugeshreyashmnnit@gmail.com>
2026-07-03 23:49:58 +0530

    Solving CORS error

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
